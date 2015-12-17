e8208e38cea669b83312f40f36a9ec6d129d6f47
gruver <gruver97@gmail.com>
2015-12-17 11:44:13 +0300

    Пустая разметка.

---
+++ VendingMachine/VendingMachine/ViewModel/VendingMachineViewModel.cs
@@ -0,0 +1,12 @@
+using GalaSoft.MvvmLight;
+
+namespace VendingMachine.ViewModel
+{
+    public class VendingMachineViewModel : ViewModelBase
+    {
+        public VendingMachineViewModel()
+        {
+            
+        }
+    }
+}
