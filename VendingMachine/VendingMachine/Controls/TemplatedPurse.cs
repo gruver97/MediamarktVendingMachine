dd482c1c3f26c5a8ab1239b7679fffcb89ff20b8
Станислав <gruver97@outlook.com>
2015-12-19 11:56:41 +0300

    Styling

---
+++ VendingMachine/VendingMachine/Controls/TemplatedPurse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using VendingMachine.VendingComponents.Coins;
+using VendingMachine.Views;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace VendingMachine.Controls
+{
+    public sealed class TemplatedPurse : Control
+    {
+        public static readonly DependencyProperty CoinGroupsProperty = DependencyProperty.Register(
+            "CoinGroups", typeof (ObservableCollection<CoinGroup>), typeof (TemplatedPurse), new PropertyMetadata(default(ObservableCollection<CoinGroup>)));
+
+        public ObservableCollection<CoinGroup> CoinGroups
+        {
+            get { return (ObservableCollection<CoinGroup>) GetValue(CoinGroupsProperty); }
+            set { SetValue(CoinGroupsProperty, value); }
+        }
+
+        public TemplatedPurse()
+        {
+            DefaultStyleKey = typeof (TemplatedPurse);
+        }
+    }
+}
